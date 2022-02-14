74a9b2f33f24ca4dc81faaedeb1922606524c5dd
Huozz <573654159@qq.com>
2022-02-14 20:21:01 +0800

    testing page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <router-view/> -->
-    <span>我是测试博客页面</span>
-    <!-- <a href="https://www.bilibili.com" _target="blank">我是外部链接到bilibi</a> -->
+    <img src="./assets/logo.png"> -->
+      <router-view/>
   </div>
 </template>
 
